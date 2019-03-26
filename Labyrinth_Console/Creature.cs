b5282fd71acaba604fa5d7a6100d6737e4cbcafc
TJW <tjameswitt@gmail.com>
2019-03-25 22:13:01 -0400

    Creature rewrite, part one

--- Labyrinth_Console/Creature.cs
+++ Labyrinth_Console/Creature.cs
@@ -6,260 +6,61 @@ using System.Threading.Tasks;
 
 namespace Labyrinth_Console
 {
-    interface ICreature
+    class Creature
     {
-        //property signatures
-        #region property signatures
-        //hp, hpmax, mp, mpmax, atk, def, speed
-        string name
-        {
-            get;
-            set;
-        }
-
-        string id
-        {
-            get;
-            set;
-        }
-
-        int level
-        {
-            get;
-            set;
-        }
+        public string name;
+        public string id;
+        public int level;
+        public Vitals vitals;
+        public bool isPlayer;
+        public MagicLearned magic;
+        public ElementalResistance eRes;
+        public PhysicalResistance pRes;
         
-        int hpmax
-        {
-            get;
-            set;
-        }
 
-        int hp
-        {
-            get;
-            set;
-        }
-
-        int mpmax
-        {
-            get;
-            set;
-        }
-
-        int mp
-        {
-            get;
-            set;
-        }
-
-        int atk
-        {
-            get;
-            set;
-        }
-
-        int def
-        {
-            get;
-            set;
-        }
-
-        int speed
-        {
-            get;
-            set;
-        }
-        bool isPlayer
-        {
-            get;
-            set;
-        }
-        MagicLearned magic
-        {
-            get;
-            set;
-        }
-        #endregion
-    }
-    
-    public class Creature: ICreature
-    {
-        //Fields
-        private string _name;
-        private string _id;
-        private int _level;
-        private int _hp;
-        private int _hpmax;
-        private int _mp;
-        private int _mpmax;
-        private int _atk;
-        private int _def;
-        private int _speed;
-        private bool _isPlayer;
-        private MagicLearned _magic;
-
-        //constructor
         public Creature(string name, string id, int level, int hpmax, int mpmax, int atk, int def, int speed, bool isPlayer, MagicLearned magic)
         {
-            _name = name;
-            _id = id;
-            _level = level;
-            _hpmax = hpmax;
-            _hp = hpmax;
-            _mpmax = mpmax;
-            _mp = mpmax;
-            _atk = atk;
-            _def = def;
-            _speed = speed;
-            _isPlayer = isPlayer;
-            _magic = magic;
+            this.name = name;
+            this.id = id;
+            this.level = level;
+            this.vitals.hpmax = hpmax;
+            this.vitals.hp = hpmax;
+            this.vitals.mpmax = mpmax;
+            this.vitals.mp = mpmax;
+            this.vitals.atk = atk;
+            this.vitals.def = def;
+            this.vitals.speed = speed;
+            this.isPlayer = isPlayer;
+            this.magic = magic;
         }
 
-        //Property implimentation
-        public string name
+        public Creature(string name, string id, Vitals vitals, bool isPlayer, MagicLearned magic)
         {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                _name = value;
-            }
+            this.name = name;
+            this.id = id;
+            this.vitals = vitals;
+            this.isPlayer = isPlayer;
+            this.magic = magic;
         }
 
-        public string id
+        public Creature()
         {
-            get
-            {
-                return _id;
-            }
-            set
-            {
-                _id = value;
-            }
+            this.name = "NULL";
+            this.id = "NULL_ID";
+            this.vitals.hpmax = 1;
+            this.vitals.hp = vitals.mpmax;
+            this.vitals.mpmax = 1;
+            this.vitals.mp = vitals.mpmax;
+            this.vitals.atk = 25;
+            this.vitals.def = 25;
+            this.vitals.speed = 30;
+            this.isPlayer = false;
+
         }
 
-        public int level
+        public void SetStatsFromCSV()
         {
-            get
-            {
-                return _level;
-            }
-            set
-            {
-                _level = value;
-            }
-        }
-
-        public int hpmax
-        {
-            get
-            {
-                return _hpmax;
-            }
-            set
-            {
-                _hpmax = value;
-            }
-        }
-
-        public int hp
-        {
-            get
-            {
-                return _hp;
-            }
-            set
-            {
-                _hp = value;
-            }
-        }
-
-        public int mpmax
-        {
-            get
-            {
-                return _mpmax;
-            }
-            set
-            {
-                _mpmax = value;
-            }
-        }
-
-        public int mp
-        {
-            get
-            {
-                return _mp;
-            }
-            set
-            {
-                _mp = value;
-            }
-        }
-
-        public int atk
-        {
-            get
-            {
-                return _atk;
-            }
-            set
-            {
-                _atk = value;
-            }
-        }
-
-        public int def
-        {
-            get
-            {
-                return _def;
-            }
-            set
-            {
-                _def = value;
-            }
-        }
-
-        public int speed
-        {
-            get
-            {
-                return _speed;
-            }
-            set
-            {
-                _speed = value;
-            }
-        }
-
-        public bool isPlayer
-        {
-            get
-            {
-                return _isPlayer;
-            }
-            set
-            {
-                _isPlayer = value;
-            }
-        }
-
-        public MagicLearned magic
-        {
-            get
-            {
-                return _magic;
-            }
-            set
-            {
-                _magic = value;
-            }
+            // To be implemented
         }
     }
 }
-
--- Labyrinth_Console/Structs.cs
+++ Labyrinth_Console/Structs.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Labyrinth_Console
 {
+    #region mapping
     public struct DestinationCoordinates
     {
         public int iDestination;
@@ -31,4 +32,75 @@ namespace Labyrinth_Console
             j = _j;
         }
     }
+    #endregion
+
+    #region creatures
+    public struct ElementalResistance
+    {
+        // Percentages
+        public int earth;
+        public int water;
+        public int wind;
+        public int fire;
+        public int ice;
+        public int thunder;
+        
+        public ElementalResistance(int earth, int water, int wind, int fire, int ice, int thunder)
+        {
+            this.earth = earth;
+            this.water = water;
+            this.wind = wind;
+            this.fire = fire;
+            this.ice = ice;
+            this.thunder = thunder;
+        }
+        
+        static readonly ElementalResistance standardResistance = new ElementalResistance(100, 100, 100, 100, 100, 100);
+
+        // Shorthand for writing @@ElementalResistance(100, 100, 100, 100, 100, 100)@@
+        public static ElementalResistance standard { get { return standardResistance; } }
+    }
+
+    public struct PhysicalResistance
+    {
+        // Percentages
+        public int blunt;
+        public int slashing;
+        public int piercing;
+
+        public PhysicalResistance (int blunt, int slashing, int piercing)
+        {
+            this.blunt = blunt;
+            this.slashing = slashing;
+            this.piercing = piercing;
+        }
+
+        static readonly PhysicalResistance standardResistance = new PhysicalResistance(100, 100, 100);
+
+        // Shorthand for writing @@PhysicalResistance(100, 100, 100)@@
+        public static PhysicalResistance standard { get { return standardResistance; } }
+    }
+
+    public struct Vitals
+    {
+        public int hpmax;
+        public int hp;
+        public int mpmax;
+        public int mp;
+        public int atk;
+        public int def;
+        public int speed;
+
+        public Vitals(int hp, int mp, int atk, int def, int speed)
+        {
+            this.hpmax = hp;
+            this.hp = hp;
+            this.mpmax = mp;
+            this.mp = mp;
+            this.atk = atk;
+            this.def = def;
+            this.speed = speed;
+        }
+    }
+    #endregion
 }
